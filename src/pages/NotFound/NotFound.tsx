6dc266052df1cc91b58a32b58705c7db025a319a
Giuseppe Messina <giuseppe.messina@chas.co.uk>
2025-08-05 12:54:48 +0100

    logic and style finished, for now. testing and accessibility next

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,20 +1,14 @@
 import React from "react";
 import styles from "./NotFound.module.css";
 import { useNavigate } from "react-router-dom";
-import { Button, Typography } from "@react-lab-mono/ui";
 
 const NotFound: React.FC = () => {
   const navigate = useNavigate();
 
   return (
     <div className={styles.container}>
-      <Typography type="h2">We can’t find that page!</Typography>
-      <Button
-        onClick={() => navigate("/")}
-        text="Go Back Home"
-        fillMode="outline"
-        variant="error"
-      />
+      <h2>This page doesn't exist!</h2>
+      <button onClick={() => navigate("/")}>Go Back Home</button>
     </div>
   );
 };
